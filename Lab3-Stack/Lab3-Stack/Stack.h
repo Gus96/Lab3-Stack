dafb48deeb08372ede24aec3b618620271e21f2e
Gus96 <dmitrij.pushckariov.2018@mail.ru>
2018-11-20 22:19:07 +0300

    Some changes

--- Lab3-Stack/Lab3-Stack/Stack.h
+++ Lab3-Stack/Lab3-Stack/Stack.h
@@ -9,6 +9,10 @@ public:
 	TStack(const int MS = 10);
 	TStack(const TStack <T> &tmp);
 	~TStack();
+	TStack& operator= (const TStack &tmp);
+	void Push(const int elem);
+	T Pop();
+	T Top() const;
 
 };
 template <class T>
@@ -31,4 +35,42 @@ template <class T>
 TStack <T> ::~TStack() 
 {
 	delete[] mas;
+}
+
+template <class T>
+TStack <T> &TStack <T> ::operator= (const TStack &tmp)
+{
+	if (MaxSize != tmp.MaxSize)
+	{
+		delete[] Stack;
+		Stack = new T[tmp.MaxSize];
+	}
+	MaxSize = tmp.MaxSize;
+	Size = tmp.Size;
+	for (int i = 0; i < Size; i++)
+		Stack[i] = tmp.Stack[i];
+	return *this;
+}
+template <class T>
+void TStack<T> ::Push(const int elem)
+{
+	if (MaxSize == Size)
+		throw "Stack is full";
+	else
+	{
+		Stack[Size] = elem;
+		Size++;
+	}
+}
+
+template <class T>
+T TStack <T>::Pop()
+{
+	if (Size == 0)
+		throw "Stack is empty";
+	else
+	{
+		Size--;
+		return Stack[Size];
+	}
 }
